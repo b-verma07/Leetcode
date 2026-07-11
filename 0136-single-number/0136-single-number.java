10ac65aba0412ea89a05ee873e9fba140f5a056e
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-11 16:39:56 +0530

    Implement brute force method for single number problem
    Added a brute force approach to find the single number in an array.

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,3 +1,4 @@
+// Optimal appraoch(XOR)
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums.length;
@@ -11,6 +12,25 @@ class Solution {
             return count;
     }
   
+}
+
+// Brute force 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int count = 0;
+        
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+                if (count == 1)
+                    return nums[i] ;
+        }
+        return 0;
+    }
 }
 
 // Synced seamlessly with LeetHub Pro
