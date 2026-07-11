f3dfb8d0874e5c6154ac6cd62f4a05bb6884f045
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-11 16:38:42 +0530

    Time: 1 ms (99.97%), Space: 46.8 MB (82.08%) - LeetHub - Time: O(?), Space: O(?)

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums.length;
-        for(int i=0; i<n; i++){
-            int count = 0;
-        
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-                if (count == 1)
-                    return nums[i] ;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            count = count ^ nums[i];
+
         }
-        return 0;
+            return count;
     }
+  
 }
 
 // Synced seamlessly with LeetHub Pro
