b87a0b7f5f04e986f72183309ac61bf5be57a2ea
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-15 00:30:02 +0530

    Implement maxProfit method using for loop

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -12,6 +12,22 @@ class Solution {
     }
 }
 
+//  with normal for loop 
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int n = prices.length;
+        int profit = 0;
+        int min = prices[0];
+        for (int i=0 ;i<n ; i++) {
+            int diff = prices[i] - min;
+            profit = Math.max(profit, diff);
+            min = Math.min(min, prices[i]);
+        }
+
+        return profit;
+    }
+}
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
