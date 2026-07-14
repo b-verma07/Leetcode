4854ccca36cc586772faaafc30a267505cf8d8e4
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-15 00:27:15 +0530

    Time: 1 ms (99.94%), Space: 94.5 MB (33.09%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n= prices.length;
+        int profit =0;
+        int min = prices[0];
+        for (int num:prices){
+            int diff= num-min;
+            profit= Math.max(profit,diff);
+            min= Math.min(min,num);
+        }
+    return profit;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
