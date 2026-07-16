c89a224660ae4e3ed6b3eec1d7bda37511b37dc0
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-16 22:30:04 +0530

    Implement brute force method for max subarray problem
    Added a brute force solution for finding the maximum subarray.

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,3 +1,4 @@
+// Kadane algorithm 
 class Solution {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
@@ -12,6 +13,22 @@ class Solution {
     }
 }
 
+// Brute force 
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int result = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            int current_sum = 0;
+            for (int j = i; j < n; j++) {
+                current_sum += nums[j];
+                result = Math.max(result, current_sum);
+            }
+        }
+        return result;
+    }
+}
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
