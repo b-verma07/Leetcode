8ee52d071c347083f882844df3363b1217a2c658
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-18 00:36:25 +0530

    Implement brute force method for majority element
    Added a brute force solution for finding the majority element.

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,5 @@
+// Optimal solution
+
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
@@ -30,6 +32,25 @@ class Solution {
     }
 }
 
+// Brute force
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n= nums.length;
+
+        for(int i=0; i<n; i++){
+        int count=0;
+             for(int j =i; j<n ;j++){
+                if(nums[i]==nums[j] ){
+                    count++;
+                }
+             }
+             if(count > (n/2)){
+        return nums[i];
+             }
+        }
+    return -1;
+    }
+}
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
