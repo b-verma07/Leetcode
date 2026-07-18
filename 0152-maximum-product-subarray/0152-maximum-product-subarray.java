0608e462b6547f5c12fd7cb5357aa939fb6a313d
Bhupender_verma <74949831+b-verma07@users.noreply.github.com>
2026-07-19 02:03:05 +0530

    Refactor maxProduct method for optimal performance
    Refactored maxProduct method to use an optimal approach for finding the maximum product of a subarray. Added handling for negative numbers and updated variable management.

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,3 +1,4 @@
+//Brute force
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
@@ -14,6 +15,24 @@ class Solution {
     }
 }
 
+// Optimal approach 
+   int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
+        int answer = nums[0];
+      
+        for (int i = 1; i < n; i++) {
+            int current =nums[i];
+            if (current < 0){
+                int temp= max;
+                max=min;
+                min=temp;
+            }
+            max = Math.max(current ,current * max);
+            min = Math.min(current, current * min);
+            answer = Math.max(answer,max);
+        }
+        return answer;
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
